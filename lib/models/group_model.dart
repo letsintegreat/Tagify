9f841d2083fd86675ed988a265abb2367197a194
chandansgowda <chandansuresh007@gmail.com>
2022-12-07 09:45:24 +0530

    added groupID parameter in Group Model

--- lib/models/group_model.dart
+++ lib/models/group_model.dart
@@ -4,22 +4,26 @@ import 'package:hackathon_project/models/user_model.dart';
 class GroupModel {
   String name;
   String logic;
-  List<MessageModel> messages=[];
-  List<String> users=[];
+  String groupId;
+  List<MessageModel> messages = [];
+  List<String> users = [];
   GroupModel({
     required this.name,
     required this.logic,
+    required this.groupId,
   });
-  Map<String, dynamic> toJson () {
+  Map<String, dynamic> toJson() {
     return {
+      'groupId': groupId,
       'name': name,
       'logic': logic,
       'messages': messages.map((e) => e.toJson()).toList(),
       'users': users.join(","),
     };
   }
-  static GroupModel fromJson (Map<String, dynamic> data) {
-    GroupModel newGroup = GroupModel(name: data['name'], logic: data['logic']);
+
+  static GroupModel fromJson(Map<String, dynamic> data) {
+    GroupModel newGroup = GroupModel(groupId: data['groupId'],name: data['name'], logic: data['logic']);
     data['messages'].forEach((e) {
       newGroup.messages.add(MessageModel.fromJson(e));
     });

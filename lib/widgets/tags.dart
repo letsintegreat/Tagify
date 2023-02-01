d4f88c8b7e28f781f85cbf460a9393b9e0a03c90
omlay <khandwalaom@gmail.com>
2023-02-01 20:22:14 +0530

    Resolves Issue # 40 tags not visible if long

--- lib/widgets/tags.dart
+++ lib/widgets/tags.dart
@@ -8,22 +8,18 @@ class Tag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 42,
-      width: 100,
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-      decoration: const BoxDecoration(
-          color: Color.fromRGBO(82, 45, 174, 1),
-          borderRadius: BorderRadius.all(Radius.circular(36))),
-      child: Center(
-        child: FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Text(
-            tagName,
-            style: GoogleFonts.inter(color: Colors.white, fontSize: 16),
-          ),
-        ),
-      ),
-    );
+    return
+      Flexible(
+        child: Container(
+        padding: const EdgeInsets.symmetric(horizontal:12, vertical: 10),
+        decoration: const BoxDecoration(
+            color: Color.fromRGBO(82, 45, 174, 1),
+            borderRadius: BorderRadius.all(Radius.circular(36))),
+              child: Text(
+                tagName,
+                style: GoogleFonts.inter(color: Colors.white, fontSize: 16),
+              )
+          )
+      );
   }
 }
